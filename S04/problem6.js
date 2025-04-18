bbdff0ad8a899f29eea1476580e38a365f1b8a7a
Rakesh Thaokar <rakeshthaokar99@gmail.com>
2025-04-18 14:38:32 +0530

    added S04 problem6

---
+++ S04/problem6.js
@@ -0,0 +1,36 @@
+const products = [
+    { name: "Laptop", price: 1200, category: "Electronics" },
+    { name: "Phone", price: 800, category: "Electronics" },
+    { name: "Shirt", price: 40, category: "Clothing" },
+    { name: "Book", price: 20, category: "Stationery" },
+    { name: "Shoes", price: 100, category: "Clothing" }
+];
+
+console.log("------------1) map()------------")
+let ProductsName = products.map((ele) => {
+    return ele.name.toUpperCase()
+})
+console.log(ProductsName)
+
+console.log("------------2) filter()------------")
+let Filtered = products.filter((ele) => {
+    return ele.category === "Clothing"
+})
+console.log(Filtered)
+
+console.log("------------3) sort()------------")
+let Sorting = products.sort((ele1, ele2) => {
+    return ele1.price - ele2.price
+}).map(ele => ele.name)
+console.log(Sorting)
+
+console.log("------------4) forEach()------------")
+let Sentence = products.forEach(ele => {
+    console.log(`The product ${ele.name} costs $${ele.price} and belongs to ${ele.category} category`)
+})
+
+console.log("------------5) reduce()------------")
+let Total = products.reduce((acc, curr) => {
+    return acc + curr.price
+}, 0)
+console.log(Total)
