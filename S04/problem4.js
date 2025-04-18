90ceb24c2d33c9d82b13faa8f931de66370d620d
Rakesh Thaokar <rakeshthaokar99@gmail.com>
2025-04-18 13:23:37 +0530

    added S04 problem4

---
+++ S04/problem4.js
@@ -0,0 +1,19 @@
+const profile = {
+    name: "Charlie",
+    age: 29,
+    address: {
+        city: "San Francisco",
+        zipcode: "94101"
+    }
+};
+
+const updates = {
+    age: 30,
+    address: {
+        zipcode: "94109",
+        country: "USA"
+    }
+};
+
+let MergeProfile = { ...profile, ...updates, address: { ...profile.address, ...updates.address } }
+console.log(MergeProfile)
